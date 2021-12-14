670ca559caad80e366179d91e6c9c9f835143db6
Karl Werner <info@zap.de.com>
2021-12-14 11:32:19 +0100

    add notification consumer in ui, first endpoint

---
+++ ui/src/notification.consumer.pact.fixtures.ts
@@ -0,0 +1,57 @@
+import { RequestOptions, ResponseOptions } from '@pact-foundation/pact';
+import { eachLike } from '@pact-foundation/pact/src/dsl/matchers';
+
+import {
+  NotificationType,
+  WebhookNotification,
+} from './notification/notificationConfig';
+import { NotificationApiReadModel } from './notification/notificationRest';
+
+export const exampleNotificationConfig: WebhookNotification = {
+  id: 1,
+  pipelineId: 1,
+  condition: 'true',
+  type: NotificationType.WEBHOOK,
+  parameters: {
+    url: 'https://url.to.webhook.com/hook',
+  },
+};
+
+export const exampleNotificationConfigReadModel: NotificationApiReadModel = {
+  id: 1,
+  pipelineId: 1,
+  condition: 'true',
+  type: NotificationType.WEBHOOK,
+  parameter: {
+    url: 'https://url.to.webhook.com/hook',
+  },
+};
+
+export function getNotificationsForPipelineRequestTitle(
+  pipelineId: number,
+): string {
+  return `a request for getting all notification configs for the pipeline with id ${pipelineId}`;
+}
+
+export function getNotificationsForPipelineRequest(
+  pipelineId: number,
+): RequestOptions {
+  return {
+    method: 'GET',
+    path: `/configs`,
+    query: {
+      pipelineId: pipelineId.toString(),
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+}
+
+export const getNotificationsForPipelineSuccessReponse: ResponseOptions = {
+  status: 200,
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+  },
+  body: eachLike(exampleNotificationConfigReadModel),
+};
---
+++ ui/src/notification.consumer.pact.test.ts
@@ -0,0 +1,49 @@
+import path from 'path';
+
+import { JestPactOptions, pactWith } from 'jest-pact';
+
+import {
+  exampleNotificationConfig,
+  getNotificationsForPipelineRequest,
+  getNotificationsForPipelineRequestTitle,
+  getNotificationsForPipelineSuccessReponse,
+} from './notification.consumer.pact.fixtures';
+import { NotificationRest } from './notification/notificationRest';
+
+const options: JestPactOptions = {
+  consumer: 'UI',
+  provider: 'notification',
+  dir: path.resolve(process.cwd(), '..', 'pacts'),
+  logDir: path.resolve(process.cwd(), '..', 'pacts', 'logs'),
+  pactfileWriteMode: 'overwrite',
+};
+
+pactWith(options, (provider) => {
+  let restService: NotificationRest;
+
+  beforeAll(() => {
+    const notificationServiceUrl = provider.mockService.baseUrl;
+    restService = new NotificationRest(notificationServiceUrl);
+  });
+
+  describe('get all notification configs for a pipeline', () => {
+    describe('when notification configs for this pipeline exist', () => {
+      const pipelineId = 1;
+
+      beforeEach(async () => {
+        await provider.addInteraction({
+          state: `notification configs for pipeline ${pipelineId} exist`,
+          uponReceiving: getNotificationsForPipelineRequestTitle(pipelineId),
+          withRequest: getNotificationsForPipelineRequest(pipelineId),
+          willRespondWith: getNotificationsForPipelineSuccessReponse,
+        });
+      });
+
+      it('returns an array containing the notification configs', async () => {
+        const configs = await restService.getAllByPipelineId(pipelineId);
+
+        expect(configs).toStrictEqual([exampleNotificationConfig]);
+      });
+    });
+  });
+});
